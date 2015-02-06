3f42936b740d6017495bd66392e7faf7edc70264
Ben Mathews <jahnsenn@gmail.com>
2015-02-06 12:14:42 -0600

    More Coin

--- Measurer/AreaMeasurer.java
+++ Measurer/AreaMeasurer.java
@@ -3,7 +3,7 @@ import java.awt.Rectangle;
 /**
    Objects of this class measure rectangles by area.
 */
-public class AreaMeasurer implements Measurer
+public class AreaMeasurer implements Measurer // can add <Rectangle> here to be able to pass a Rectangle in place of an Object
 {
    public double measure(Object anObject)
    {
